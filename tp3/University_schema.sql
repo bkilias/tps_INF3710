257f54f6b602b7813757becdfcd2df91132548fe
Selamet53 <selamet.zamanoglu@polymtl.ca>
2023-03-07 18:51:27 -0500

    partie 1: changement des types des données

--- tp3/University_schema.sql
+++ tp3/University_schema.sql
@@ -2,47 +2,47 @@ CREATE DOMAIN SexType AS CHAR
 CHECK (VALUE IN ('M', 'F', 'NB'));
 
 CREATE TABLE Etudiant (
-	sid integer PRIMARY KEY,
+	sid char(2) PRIMARY KEY,
 	snom char(50),
 	sexe SexType,
 	dateNaissance date,
-	niveau integer,
-	moyenne float(4)
+	niveau char(1),
+	moyenne char(3)
 );
 
 CREATE TABLE Dept (
-	dID integer PRIMARY KEY,
+	dID char(25) PRIMARY KEY,
 	nombrephds integer
 );
 
 CREATE TABLE Prof (
-	pID integer PRIMARY KEY,
-	pnom char(50),
-	dep integer NOT NULL UNIQUE, --Ajout de UNIQUE pour répondre à la question 1.2
+	pID char(2) PRIMARY KEY,
+	pnom char(2),
+	dep char(25) NOT NULL UNIQUE, --Ajout de UNIQUE pour répondre à la question 1.2
 	FOREIGN KEY(dep) REFERENCES Dept(dID)
 );
 
 CREATE TABLE Cours (
-	cno integer PRIMARY KEY,
+	cno char(3) PRIMARY KEY,
 	cnom char(50),
-	dep integer NOT NULL,
+	dep char(25) NOT NULL,
 	FOREIGN KEY(dep) REFERENCES dept(dID)
 );
 
 CREATE TABLE section (
-	cno integer NOT NULL,
-	sectno integer NOT NULL,
-	pID integer,
+	cno char(3) NOT NULL,
+	sectno char(1) NOT NULL,
+	pID char(2),
 	PRIMARY KEY(cno, sectno),
 	FOREIGN KEY(pID) REFERENCES Prof(pID) ON DELETE SET NULL,
 	FOREIGN KEY(cno) REFERENCES Cours(cno) ON DELETE CASCADE 
 ); 
 
 CREATE TABLE Inscription (
-	sid integer NOT NULL,
-	cno integer NOT NULL,
-	sectno integer NOT NULL,
-	note numeric(3, 2),
+	sid char(2) NOT NULL,
+	cno char(3) NOT NULL,
+	sectno char(1) NOT NULL,
+	note integer,
 	PRIMARY KEY(sid, cno, sectno),
 	FOREIGN KEY(sid) REFERENCES Etudiant(sid),
 	FOREIGN KEY(cno, sectno) REFERENCES section(cno, sectno)
